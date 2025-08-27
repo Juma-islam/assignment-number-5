0d55c003b6a7e395a4ad8ff5a3c38b50f814b88d
Juma-islam <ononnaislamj@gmail.com>
2025-08-27 17:19:41 +0600

    added history section functionality

--- script/index.js
+++ script/index.js
@@ -12,5 +12,31 @@ for(let heartClick of heartClicks){
 
 
 // call button er kaj-2
-const callButton = document.getElementsByClassName('call-btn');
-console.log();
+const callButtons=document.getElementsByClassName('call-btn');
+const coinBtn=document.getElementById('coin-btn');
+let coin=100
+for(let callButton of callButtons){
+    callButton.addEventListener('click', function(){
+        const parent = callButton.parentElement; 
+        const serviceName=parent.querySelector('.service-name').innerText;
+        const serviceNumber=parent.querySelector('.service-number').innerText;
+
+        if(coin<20){
+    alert('❌ Not enough coins to make a call');
+    return;
+}
+coin -=20;
+coinBtn.innerText = coin;
+alert(`📞 Calling  ${serviceName}  ${serviceNumber}`)
+const callHistory = document.getElementById('call-history');
+const newCart = document.createElement('div');
+newCart.innerHTML = ` <div class=" bg-white  mt-9 ">
+<h2 class="font-bold font-hind">${serviceName}</h2>
+<h2 class="font-bold mt-4 text-xl font-roboto ">${serviceNumber}</h2>
+                     </div>`
+callHistory.append(newCart);
+
+
+    })
+}
+
