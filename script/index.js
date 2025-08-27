44c3d55806fad25fbfddd8e7aa7acc8cec80243e
Juma-islam <ononnaislamj@gmail.com>
2025-08-27 19:44:53 +0600

    copy functionality added

--- script/index.js
+++ script/index.js
@@ -1,46 +1,68 @@
 // heart a click korle increase-1
-const heartClicks=document.getElementsByClassName('heart-click');
-const heartBtn=document.getElementById('heart-btn');
-let count=0;
-for(let heartClick of heartClicks){
-    heartClick.addEventListener('click', function(){
+const heartClicks = document.getElementsByClassName('heart-click');
+const heartBtn = document.getElementById('heart-btn');
+let count = 0;
+for (let heartClick of heartClicks) {
+    heartClick.addEventListener('click', function () {
         count++;
         heartBtn.innerText = count;
     })
-    
+
 }
 
 
 // call button er kaj-2
-const callButtons=document.getElementsByClassName('call-btn');
-const coinBtn=document.getElementById('coin-btn');
-let coin=100
-for(let callButton of callButtons){
-    callButton.addEventListener('click', function(){
-        const parent = callButton.parentElement; 
-        const serviceName=parent.querySelector('.service-name').innerText;
-        const serviceNumber=parent.querySelector('.service-number').innerText;
+const callButtons = document.getElementsByClassName('call-btn');
+const coinBtn = document.getElementById('coin-btn');
+let coin = 100
+for (let callButton of callButtons) {
+    callButton.addEventListener('click', function () {
+        const parent = callButton.parentElement;
+        const serviceName = parent.querySelector('.service-name').innerText;
+        const serviceNumber = parent.querySelector('.service-number').innerText;
 
-        if(coin<20){
-    alert('❌ Not enough coins to make a call');
-    return;
-}
-coin -=20;
-coinBtn.innerText = coin;
-alert(`📞 Calling  ${serviceName}  ${serviceNumber}`)
-const callHistory = document.getElementById('call-history');
-const newCart = document.createElement('div');
-newCart.innerHTML = ` <div class=" bg-white  mt-9 ">
+        if (coin < 20) {
+            alert('❌ Not enough coins to make a call');
+            return;
+        }
+        coin -= 20;
+        coinBtn.innerText = coin;
+        alert(`📞 Calling  ${serviceName}  ${serviceNumber}`)
+        const callHistory = document.getElementById('call-history');
+        const newCart = document.createElement('div');
+        newCart.innerHTML = ` <div class=" bg-white  mt-9 ">
 <h2 class="font-bold font-hind">${serviceName}</h2>
 <h2 class="font-bold mt-4 text-xl font-roboto ">${serviceNumber}</h2>
                      </div>`
-                     
-callHistory.append(newCart);
-const now= new Date();
-                     const timeString=now.toLocaleTimeString();
-                     
-                     callHistory.append(timeString)
+        // time Setting 
+        const now = new Date();
+        const timeElement = document.createElement('p');
+        timeElement.innerText = `${now.toLocaleTimeString()}`;
+        newCart.appendChild(timeElement);
+        callHistory.appendChild(newCart);
+        const clearButton = document.getElementById('clear-btn')
+        clearButton.addEventListener('click', function () {
+            newCart.style.display = 'none';
+            timeElement.style.display = 'none';
 
+        })
     })
+}
+
+// copy clicked
+const copyClicks = document.getElementsByClassName('copy-click');
+const copyBtn = document.getElementById('copy-btn');
+let counts = 0;
+for (let copyClick of copyClicks) {
+    copyClick.addEventListener('click', function () {
+        counts++;
+        copyBtn.innerText = counts;
+        
+         const parent =copyClick.parentElement;
+        const serNum = parent.querySelector('.service-number').innerText;
+     navigator.clipboard.writeText(serNum);
+alert(`this number is copied ${serNum}`);
+    })
+
 }
 
