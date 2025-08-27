8f2cb80852b962814a98ce5d951ca7e121cc2b9e
Juma-islam <ononnaislamj@gmail.com>
2025-08-27 13:44:36 +0600

    added heart a click and increase heart number function

--- script/index.js
+++ script/index.js
@@ -1,1 +1,16 @@
-console.log('hello');
+// heart a click korle increase-1
+const heartClicks=document.getElementsByClassName('heart-click');
+const heartBtn=document.getElementById('heart-btn');
+let count=0;
+for(let heartClick of heartClicks){
+    heartClick.addEventListener('click', function(){
+        count++;
+        heartBtn.innerText = count;
+    })
+    
+}
+
+
+// call button er kaj-2
+const callButton = document.getElementsByClassName('call-btn');
+console.log();
